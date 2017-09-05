4c1273aee5c1a439bede95f6f6379ab2bbb5d999
Eric Hutson <ehutson@gmail.com>
2017-09-04 21:34:35 -0400

    adding some POC code

---
+++ src/main/java/com/ehutson/test1/RocksDbSample.java
@@ -0,0 +1,64 @@
+package com.ehutson.test1;
+
+import java.lang.IllegalArgumentException;
+
+import org.rocksdb.*;
+import org.rocksdb.util.SizeUnit;
+
+public class RocksDbSample {
+
+	static {
+		RocksDB.loadLibrary();
+	}
+
+	public static void main(final String[] args) {
+
+		final String dbPath = "/tmp/db";
+
+		System.out.println("RocksDBSample");
+
+		try (final Options options = new Options();
+				final Filter bloomFilter = new BloomFilter(10);
+				final ReadOptions readOptions = new ReadOptions().setFillCache(false);
+				// final Statistics stats = new Statistics();
+				final RateLimiter rateLimiter = new RateLimiter(10000000, 10000, 10);) {
+
+			try {
+				options.setCreateIfMissing(true).setWriteBufferSize(8 * SizeUnit.KB).setMaxWriteBufferNumber(3)
+						.setMaxBackgroundCompactions(10).setCompressionType(CompressionType.SNAPPY_COMPRESSION)
+						.setCompactionStyle(CompactionStyle.UNIVERSAL);
+
+				options.setMemTableConfig(new HashLinkedListMemTableConfig().setBucketCount(100000));
+
+				final BlockBasedTableConfig tableOptions = new BlockBasedTableConfig();
+				tableOptions.setBlockCacheSize(64 * SizeUnit.KB).setFilter(bloomFilter).setCacheNumShardBits(6)
+						.setBlockSizeDeviation(5).setBlockRestartInterval(10).setCacheIndexAndFilterBlocks(true)
+						.setHashIndexAllowCollision(false).setBlockCacheCompressedSize(64 * SizeUnit.KB)
+						.setBlockCacheCompressedNumShardBits(10);
+
+				options.setTableFormatConfig(tableOptions);
+
+				
+				try (final RocksDB db = RocksDB.open(options,dbPath)) {
+					db.put("hello".getBytes(), "world".getBytes());
+					
+					final byte[] value = db.get("hello".getBytes());
+					System.out.println("Hello, " + value.toString());
+					
+					db.delete("hello".getBytes());
+					final byte[] value2 = db.get("hello".getBytes());
+					System.out.println("hello, " + value2.toString());
+				} catch(final RocksDBException e) {
+					System.err.println(e);
+				}
+				
+			} catch (final IllegalArgumentException e) {
+				System.err.println(e);
+			}
+
+		} catch (final Exception e) {
+			System.err.println(e);
+		}
+
+	}
+}
---
+++ src/main/java/com/ehutson/test1/RocksDbSampleSimple.java
@@ -0,0 +1,31 @@
+package com.ehutson.test1;
+
+import org.rocksdb.*;
+
+
+public class RocksDbSampleSimple {
+
+	public static void main(String[] args) {
+		System.out.println("Loading library");
+		RocksDB.loadLibrary();
+		
+		try(final Options options = new Options().setCreateIfMissing(true)) {
+			System.out.println("Creating DB");
+			try (final RocksDB db = RocksDB.open(options, "/tmp/db")) {
+				System.out.println("doing something");
+				
+				db.put("Hello".getBytes(), "World".getBytes());
+				final byte[] retval = db.get("Hello".getBytes());
+				System.out.println("Hello, " + new String(retval));
+				System.out.println("removing hello");
+				db.delete("Hello".getBytes());
+				final byte[] retval2 = db.get("Hello".getBytes());
+				assert(retval2 == null);
+			}
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+
+	}
+
+}
